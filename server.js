c7d45498d22980c72727fba84d618da7859ee13f
noureddein <noureddein@gmail.com>
2021-03-28 21:00:44 +0300

    test the server

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
+//load the express module
+const express = require('express');
+const PORT = 3000;
+// Create  aserer apllication
+const app = express();
 
+//setup a route to handle
+//handle the get request to the '/' path
+app.get('/', handleRequest);
+
+//Request are handle by callbacks
+//express will pass parameter to the callbacks
+/*
+ *req / request => All the information about the request the server receive
+ * res / response => method which can be called to create and send a response to the clint
+*/
+const handleRequest = (request, response) => {
+  console.log(request.query);
+  response.send('ok');
+};
+
+app.listen(PORT, () => {
+  console.log('listen from app.listen');
+});
