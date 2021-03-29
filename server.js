6bda8aaad10871f66402c34eff199198a6b1d6f0
noureddein <noureddein@gmail.com>
2021-03-29 17:55:08 +0300

    not solved at all

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 //load the express module
 const express = require('express');
-const PORT = 3000;
-// Create  aserer apllication
+const cors = require('cors');
+require('dotenv').config();
+
+// Create a serer apllication
 const app = express();
 
+//create port to listen to the response
+const PORT = process.env.PORT || 3000;
 
+//for securty
+app.use(cors());
 
 //Request are handle by callbacks
 //express will pass parameter to the callbacks
@@ -12,27 +18,39 @@ const app = express();
  *req / request => All the information about the request the server receive
  * res / response => method which can be called to create and send a response to the clint
 */
-const handleRequest = (request, response) => {
-  console.log(request.query);
-  response.send('<h1>About<h1>');
-};
+
+app.get('/location', handleLocation);
+
+function handleLocation(request, response) {
+  let searchForCity = request.query.city;
+  let createCityObj = getLocationInfo(searchForCity);
+  try {
+    response.status(200).send(createCityObj);
+  } catch (error) {
+    response.status(500).send(`There is somthing wrong ${error}`);
+  }
+}
 
 
-//setup a route to handle
-//handle the get request to the '/' path
-//call the handle function
-app.get('/about', handleRequest);
+//Build the Constructer function
 
-app.get('/locations', (req, res) => {
-  const locations = require('./data/location.json');
-  // console.log(req);
-  res.json(locations);
+function CreateLocationObj(city, display_name, latitude, longitude) {
+  this.search_query = city;
+  this.formatted_query = display_name;
+  this.latitude = latitude;
+  this.longitude = longitude;
+}
 
-});
+function getLocationInfo(searchForCity) {
+  const requireLocationData = require('./data/location.json');
+  let name = requireLocationData[0].display_name;
+  let latitude = requireLocationData[0].lat;
+  let longitude = requireLocationData[0].lon;
+  const obje = new CreateLocationObj(searchForCity, name, latitude, longitude);
+  return obje;
 
-
-
+}
 
 app.listen(PORT, () => {
-  console.log('listen from app.listen');
+  console.log(`listen from app.listen Port No. ${PORT}`);
 });
